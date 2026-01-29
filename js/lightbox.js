2b305babb3e6cc473f4e93ed2de803861904df78
pipelon1012 <felsal3423@gmail.com>
2026-01-29 12:32:13 -0500

    Actualización de imágenes del proyecto

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,27 +1,19 @@
-const imagenes = document.querySelectorAll('.img-galeria')
-const imagenesLight = document.querySelector('.agregar-imagen')
-const contenedorLight = document.querySelector('.imagen-light')
-const hamburguer1 = document.querySelector('.hamburguer');
+const imagenes = document.querySelectorAll('.img-galeria');
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light');
+const tituloLight = document.querySelector('.titulo-light');
 
-imagenes.forEach(imagen =>{
-    imagen.addEventListener('click',()=>{
-        
-        aparecerImagen(imagen.getAttribute('src'))
-    })
-})
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click', () => {
+        imagenLight.src = imagen.src;
+        tituloLight.textContent = imagen.dataset.title;
+        contenedorLight.classList.add('show');
+        imagenLight.classList.add('showImage');
+    });
+});
 
-contenedorLight.addEventListener('click', (e)=>{
-    if(e.target !== imagenesLight){
-        contenedorLight.classList.toggle('show')
-        imagenesLight.classList.toggle('showImage')
-        hamburguer1.style.opacity = '1'
-    }
-})
+contenedorLight.addEventListener('click', () => {
+    contenedorLight.classList.remove('show');
+    imagenLight.classList.remove('showImage');
+});
 
-
-const aparecerImagen = (imagen)=>{
-    imagenesLight.src = imagen;
-    contenedorLight.classList.toggle('show')
-    imagenesLight.classList.toggle('showImage')
-    hamburguer1.style.opacity = '0'
-}
